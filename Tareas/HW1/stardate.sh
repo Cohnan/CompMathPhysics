90c92b828b8d34ef0c247b20defbaf8d381dec10
Cohnan <sebastian.cohnan@gmail.com>
2015-06-03 23:20:15 -0500

    Update stardate.sh funcional, sin comentarios

--- Tareas/HW1/stardate.sh
+++ Tareas/HW1/stardate.sh
@@ -7,11 +7,10 @@
 
 export LC_NUMERIC="en_US.UTF-8"
 
-width=40
-separador="="
-year=${1:-1983}
-
-echo $year
+width=$(($(tput cols) / 2))
+separador="#"
+year=${1:-1996}
+cell=$((width / 3))
 
 lineaSep=$separador
 
@@ -20,15 +19,18 @@ do
 	lineaSep="$lineaSep$lineaSep$lineaSep"
 done
 
+lineaSep=${lineaSep:0:$width}
+
 echo $lineaSep
 figlet $year
-echo -e "$(awk 'BEGIN {FS="\t"};{if ($1 == $year) print $2}' worldhistory.tsv)"
+echo "$(awk 'BEGIN {FS="\t"};{if ($1 == $year) print $2}' worldhistory.tsv)"
 echo $lineaSep
 
 echo "LOOK AT THE FOLLOWING STARS:"
-printf "%-8s%-8s%-8s\n" "RA/°" "DEC/°" "HIP No."
+printf "%-${cell}s%-${cell}s%-${cell}s\n" "RA/°" "DEC/°" "HIP No."
+
 oldIFS="$IFS"
 
-printf "%-8.2f%-8.2f%-8d\n" $(awk 'BEGIN {FS=","; i=0};{if (i < 5 && 32 <= $10 && $10 <= 33) {print $8,$9,$2; i++}}' hyg.csv)
+printf "%-${cell}.2f%-${cell}.2f%-${cell}d\n" $(awk 'BEGIN {FS=","; i=0};{if (i < 5 && (2015-$year) <= $10 && $10 <= (2015-$year + 1)) {print $8,$9,$2; i++}}' hyg.csv)
 
 IFS="$oldIFS"
