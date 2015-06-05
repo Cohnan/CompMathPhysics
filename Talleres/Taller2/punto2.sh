4fcf562577db6b4ca8233a3441c96c0278622e45
Cohnan <sebastian.cohnan@gmail.com>
2015-06-05 18:39:24 -0500

    Cambios a solución de segundo taller(?)

--- Talleres/Taller2/punto2.sh
+++ Talleres/Taller2/punto2.sh
@@ -1,3 +1,3 @@
 sed 's/\.//g' pgn.dat | sed -E 's/,([0-9])/.\1/g' | sed -E 's/ ([0-9]|\()/\t\1/g' | sed 's/)//g' | sed 's/(/-/g' > pgn.tsv
 
-head -29 pgn.tsv | sort --field-separator=$'\t' -k 6,6 | head -1
+head -29 pgn.tsv | sort --field-separator=$'\t' -k 6,6 | head -1 | cut -f 1
--- Talleres/Taller2/punto3.sh
+++ Talleres/Taller2/punto3.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-#saturnoSatelites.csv | sed -E 's/ +$//g' | sed ':a;N;$!ba;s/\nN\n/\n/g'
+sed -E 's/^ +//g' saturnoSatelites.csv | sed -E 's/([0-9]),/\1/g' | sed -E 's/([0-9])R/\1/g' | sed ':a;N;$!ba;s/\n\n\n*/\n/g' | sed -E 's/  +/\t/g'> saturnoSatelitesMej.tsv
+
+gnuplot << EOF 
+set datafile separator "\t"
+set title "Satelites de Saturno: 3a ley de Kepler"
+set xlabel "Cuadrado del Tiempo de Orbita"
+set ylabel "Cubo del Semieje Mayor"
+cuad(x) = x**2
+cubo(x) = x**3
+plot "saturnoSatelitesMej.tsv" using (cuad(\$4)):(cubo(\$2))
+y(x)=m*x + b
+fit y(x) "saturnoSatelitesMej.tsv" using (cuad($4)):(cubo($2)) via m, b
+replot m*x + b
+EOF
+
